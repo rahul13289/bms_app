ab2e19cdd4e33de1ed8fcc045ae2c504c92e4a30
RAHUL BALAJI <rahul.balaji13289@gmail.com>
2025-01-15 22:51:02 +0530

    Add files via upload

---
+++ src/main/java/com/example/branchmanagementsystem/BranchmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.branchmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchmanagementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/branchmanagementsystem/BranchmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.branchmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
